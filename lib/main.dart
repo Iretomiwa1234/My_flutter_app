44cc64161fd4d1ac854249a255a3f147a3745f97
DFS0507 <adeyemooluwatoyin2018@gmail.com>
2025-06-23 17:02:14 +0100

    step-2

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 // ignore_for_file: avoid_print, use_build_context_synchronously
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:my_app/firebase_options.dart';
 import 'package:my_app/views/login_view.dart';
 import 'package:my_app/views/register_view.dart';
+import 'package:my_app/views/verify_email_view.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -34,13 +36,14 @@ class HomePage extends StatelessWidget {
       builder: (context, snapshot) {
         switch (snapshot.connectionState) {
           case ConnectionState.done:
-            // final user = FirebaseAuth.instance.currentUser;
-            // if (user?.emailVerified ?? false) {
-            //   print('You are a verified User');
-            //   return const Text('You are a verified User');
-            // } else {
-            //   return const VerifyEmailView();
-            // }
+            final user = FirebaseAuth.instance.currentUser;
+            if (user != null) {
+              if (user.emailVerified) {
+                return Text('You are a verified User');
+              } else {
+                return const VerifyEmailView();
+              }
+            }
             return const LoginView();
           default:
             return const CircularProgressIndicator();
