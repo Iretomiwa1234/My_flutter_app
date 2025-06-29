b5604505a8669c31d75bc32662bb6da7316905b6
DFS0507 <adeyemooluwatoyin2018@gmail.com>
2025-06-29 02:02:57 +0100

    step-7

--- lib/views/verify_email_view.dart
+++ lib/views/verify_email_view.dart
@@ -1,7 +1,6 @@
 // ignore_for_file: use_build_context_synchronously
 
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:my_app/constansts/routes.dart';
 
